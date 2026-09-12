f8e892dbd78abef8bbf18c2ddb07673d1a025f03
bolcca1-blip <bolcca1@gmail.com>
2026-09-12 15:39:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="domaci-panel-v9-fun-drawing";
+const CACHE="domaci-panel-v11-shared-drawing";
 const SHELL=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event=>{
